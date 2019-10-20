10f9203b582847681289b972d51716f0c35d2652
Arturo Vasquez <artav17@gmail.com>
2019-10-20 12:47:25 -0400

    SINGLETON

--- artui.class.php
+++ artui.class.php
@@ -42,6 +42,23 @@ class artui{
 	///////////////////////////////////////////////////////////////////////////
 	//IMPRIMIR CONTROLES HTML
 	//PRINTHTML
+	
+	///Condition 1 – Presence of a static member variable
+	private static $_instance = null;
+	
+	///Condition 2 – Locked down the constructor
+	private function __construct() { } //Prevent any oustide instantiation of this class
+	
+	///Condition 3 – Prevent any object or instance of that class to be cloned
+	private function __clone() { } //Prevent any copy of this object
+	
+	///Condition 4 – Have a single globally accessible static method
+	public static function getInstance(){
+		if( !is_object(self::$_instance) ) //or if( is_null(self::$_instance) ) or if( self::$_instance == null )
+		self::$_instance = new artui();
+		return self::$_instance;
+	}
+	
 	function html($cadena){
 		echo($cadena);
 		return 0;
